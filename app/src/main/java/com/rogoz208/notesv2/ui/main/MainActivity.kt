e5397012ff56bd5c6f68c530d3ee1b84b0461a59
rogoz208 <rogoz208@gmail.com>
2021-10-31 16:44:15 +0300

    added basic packages structure

--- app/src/main/java/com/rogoz208/notesv2/ui/main/MainActivity.kt
+++ app/src/main/java/com/rogoz208/notesv2/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.rogoz208.notesv2
+package com.rogoz208.notesv2.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rogoz208.notesv2.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
