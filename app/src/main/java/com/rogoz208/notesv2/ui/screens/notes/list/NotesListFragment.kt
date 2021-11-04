4f74446f9124fa997562c81e7ecbf0c118ac282b
rogoz208 <rogoz208@gmail.com>
2021-11-04 19:11:36 +0300

    lesson-2: added floating action button to NotesListFragment

--- app/src/main/java/com/rogoz208/notesv2/ui/screens/notes/list/NotesListFragment.kt
+++ app/src/main/java/com/rogoz208/notesv2/ui/screens/notes/list/NotesListFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.rogoz208.notesv2.R
 import com.rogoz208.notesv2.data.App
@@ -24,6 +25,7 @@ class NotesListFragment : Fragment(R.layout.fragment_notes_list) {
 
         initRepo()
         initRecyclerView()
+        initFloatingActionButton()
     }
 
     private fun initRepo() {
@@ -36,15 +38,37 @@ class NotesListFragment : Fragment(R.layout.fragment_notes_list) {
         binding.recyclerView.adapter = this.adapter
 
         adapter.data = repo.notes
-        adapter.setOnItemClickListener(object: OnItemClickListener {
+        adapter.setOnItemClickListener(object : OnItemClickListener {
             override fun onItemClick(item: NoteEntity, position: Int) {
-                Toast.makeText(requireContext(), "Click on ${item.title}", Toast.LENGTH_SHORT ).show()
+                Toast.makeText(requireContext(), "Click on ${item.title}", Toast.LENGTH_SHORT)
+                    .show()
             }
 
             override fun onItemLongClick(item: NoteEntity, itemView: View, position: Int) {
-                Toast.makeText(requireContext(), "Long click on ${item.title}", Toast.LENGTH_SHORT ).show()
+                Toast.makeText(requireContext(), "Long click on ${item.title}", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        })
+    }
+
+    private fun initFloatingActionButton() {
+        binding.recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                if (dy > 0 || dy < 0 && binding.addNoteFloatingActionButton.isShown) {
+                    binding.addNoteFloatingActionButton.hide()
+                }
             }
 
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                    binding.addNoteFloatingActionButton.show()
+                }
+                super.onScrollStateChanged(recyclerView, newState)
+            }
         })
+
+        binding.addNoteFloatingActionButton.setOnClickListener {
+            Toast.makeText(requireContext(), "Add a new note clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
