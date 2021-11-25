5c57e9c7298e81bb403a18060ccd2052341e26da
rogoz208 <rogoz208@gmail.com>
2021-11-25 22:29:29 +0300

    Revert "lesson-4: added url preview WebView in EditNoteActivity"
    This reverts commit 97bf9723

--- app/src/main/java/com/rogoz208/notesv2/ui/screens/notes/edit/EditNoteViewModelFactory.kt
+++ app/src/main/java/com/rogoz208/notesv2/ui/screens/notes/edit/EditNoteViewModelFactory.kt
@@ -3,12 +3,11 @@ package com.rogoz208.notesv2.ui.screens.notes.edit
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.rogoz208.notesv2.domain.repos.NotesRepo
-import com.rogoz208.notesv2.domain.repos.UrlPreviewRepo
 
-class EditNoteViewModelFactory(private val notesRepo: NotesRepo, private val urlPreviewRepo: UrlPreviewRepo) :
+class EditNoteViewModelFactory(private val repo: NotesRepo) :
     ViewModelProvider.NewInstanceFactory() {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return EditNoteViewModel(notesRepo, urlPreviewRepo) as T
+        return EditNoteViewModel(repo) as T
     }
 }
