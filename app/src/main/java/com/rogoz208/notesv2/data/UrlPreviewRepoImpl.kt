5dd6d723643245f5e05ae1c752418497eed4eceb
rogoz208 <rogoz208@gmail.com>
2021-11-25 22:25:17 +0300

    lesson-5: added url preview WebView in EditNoteActivity

---
+++ app/src/main/java/com/rogoz208/notesv2/data/UrlPreviewRepoImpl.kt
@@ -0,0 +1,38 @@
+package com.rogoz208.notesv2.data
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import com.rogoz208.notesv2.domain.repos.UrlPreviewRepo
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import java.util.stream.Collectors
+
+class UrlPreviewRepoImpl : UrlPreviewRepo {
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun getWebPageSync(urlString: String): String {
+        val result: String
+        val url = URL(urlString)
+        var connection: HttpURLConnection? = null
+        try {
+            connection = url.openConnection() as HttpURLConnection
+            connection.requestMethod = "GET"
+            connection.connectTimeout = 5_000
+
+            val bufferedReader = BufferedReader(InputStreamReader(connection.inputStream))
+            result = bufferedReader.lines().collect(Collectors.joining("\n"))
+        } finally {
+            connection?.disconnect()
+        }
+        return result
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun getWebPageAsync(urlString: String, callback: (String) -> Unit) {
+        Thread {
+            callback.invoke(getWebPageSync(urlString))
+        }.start()
+    }
+}
